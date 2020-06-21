d63c489c47589950569fb39f9a4ad98c6cef959c
Beyond14 <634539585@qq.com>
2020-06-21 12:50:06 +0800

    add DevClass

---
+++ src/main/java/com/ldh/githubproject/DevClass.java
@@ -0,0 +1,11 @@
+package com.ldh.githubproject;
+
+/**
+ * @author LiuDehuai
+ * @fileName:DevClass
+ * @Date:2020/6/21 12:48
+ * <author>     <time>     <version>    <desc>
+ * 作者姓名       修改时间    版本号       描述
+ */
+public class DevClass {
+}
