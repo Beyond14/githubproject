1456f5a0e9df701dad4e6ece196177213f9ff222
Beyond14 <634539585@qq.com>
2020-06-21 12:17:45 +0800

    create Test.java

---
+++ src/main/java/com/ldh/githubproject/Test.java
@@ -0,0 +1,14 @@
+package com.ldh.githubproject;
+
+/**
+ * @author LiuDehuai
+ * @fileName:Test
+ * @Date:2020/6/21 12:16
+ * <author>     <time>     <version>    <desc>
+ * 作者姓名       修改时间    版本号       描述
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(""Test"");
+    }
+}
