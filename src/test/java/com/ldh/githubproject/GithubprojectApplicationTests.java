3ea1b1123b020df4a307f5c9a73b207b9852d769
Beyond14 <634539585@qq.com>
2020-06-21 12:13:25 +0800

    create github project

---
+++ src/test/java/com/ldh/githubproject/GithubprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldh.githubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
